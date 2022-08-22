406f4f71a6bb0126f7093c3311dbefa95d47ff8a
etuong <etuong@gmail.com>
2022-08-22 14:42:47 -0700

    Launch configurations

--- config/database.js
+++ config/database.js
@@ -8,4 +8,16 @@ module.exports = ({ env }) => ({
     },
     useNullAsDefault: true,
   },
+  // connection: {
+  //   client: 'postgres',
+  //   connection: {
+  //     host: env('DATABASE_HOST', 'localhost'),
+  //     port: env.int('DATABASE_PORT', 5432),
+  //     database: env('DATABASE_NAME', 'blog'),
+  //     user: env('DATABASE_USERNAME', 'postgres'),
+  //     password: env('DATABASE_PASSWORD', 'password'),
+  //     schema: env('DATABASE_SCHEMA', 'public'), // Not required
+  //   },
+  //   debug: false,
+  // },
 });
--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  *  category controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::category.category');
+// Experiment
+module.exports = createCoreController(
+  "api::category.category",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const response = await strapi.entityService
+        .findMany("api::category.category")
+        .then((res) => {
+          if (res.length >= 3) {
+            return "You have already created 3 categories!";
+          }
+        });
+
+      // If not great than 3, then create category, if true return the message
+      const finalRes = (await response) ?? super.create(ctx);
+
+      return finalRes;
+    },
+  })
+);
